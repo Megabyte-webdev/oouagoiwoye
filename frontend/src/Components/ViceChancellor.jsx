74c55a3f5e61ac5bfe6136f8fd7ef73b4299f43c
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-04 15:16:48 +0100

    Update ViceChancellor.jsx

--- frontend/src/Components/ViceChancellor.jsx
+++ frontend/src/Components/ViceChancellor.jsx
@@ -8,11 +8,11 @@ const ViceChancellor = () => {
             <article className='w-full lg:w-1/2'>
                 <span className='flex flex-row items-center justify-center bg-amber-950 p-1 w-56'>
                     <i className='bx bx-buildings mr-1 text-xl text-amber-500'></i>
-                    <h5 className="text-xl font-semibold text-amber-500">Our vice-chancellor</h5>
+                    <h5 className="text-xl font-semibold text-amber-500 capitalize">vice-chancellor</h5>
                 </span>
                 {/* <h5 className="section-label"></h5> */}
 
-                <h2 className="text-white text-4xl font-bold my-3">Prof. Agboola Ayodeji <br /> olayinka johnson</h2>
+                <h2 className="text-white text-4xl font-bold my-3 capitalize">Prof. Agboola Ayodeji <br /> olayinka johnson</h2>
                 <p className='text-blue-900 font-semibold text-xl my-2'>
                     OLABISI ONABANJO University Vice-Chancellor
                 </p>
