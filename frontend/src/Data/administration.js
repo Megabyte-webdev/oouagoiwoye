887940d00acfb2132eed759280ae9e4226e5fb15
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-04 19:51:22 +0100

    Update administration.js

--- frontend/src/Data/administration.js
+++ frontend/src/Data/administration.js
@@ -3,50 +3,7 @@ import assets from "../assets/assets";
 
 
 const principalData = [
-    {
-        id: 1,
-        title: "Deputy Vice-Chancellor (Academic)",
-        name: "Prof. AGBOOLA, Ayodeji Olayinka Johnson.",
-        href: "main-campus",
-        image: assets.imaage,
-        video: "",
-        biography: "Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research, and community engagement. Over the past few years, we have nurtured leaders across a multitude of disciplines through our diverse programs. Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research, and community engagement. Over the past few years, we have nurtured leaders across a multitude of disciplines through our diverse programs.",
-        roles: [
-            "Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research.", 
-            "Olabisi Onabanjo University is a prominent institution", 
-            "Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research.", 
-            "Olabisi Onabanjo University is a prominent institution", 
-            "Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research.", 
-            "Olabisi Onabanjo University is a prominent institution"
-        ],
-        contactsInfo: {
-            whasapp: "",
-            facebook: "",
-            Youtube: ""
-        }
-    },
-    {
-        id: 2,
-        title: "Deputy Vice-Chancellor (Administration)",
-        name: "Prof. AGBOOLA, Ayodeji Olayinka Johnson.",
-        href: "main-campus",
-        image: assets.imaage,
-        video: "",
-        biography: "Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research, and community engagement. Over the past few years, we have nurtured leaders across a multitude of disciplines through our diverse programs. Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research, and community engagement. Over the past few years, we have nurtured leaders across a multitude of disciplines through our diverse programs.",
-        roles: [
-            "Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research.", 
-            "Olabisi Onabanjo University is a prominent institution", 
-            "Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research.", 
-            "Olabisi Onabanjo University is a prominent institution", 
-            "Olabisi Onabanjo University is a prominent institution committed to excellence in teaching, learning, research.", 
-            "Olabisi Onabanjo University is a prominent institution"
-        ],
-        contactsInfo: {
-            whasapp: "",
-            facebook: "",
-            Youtube: ""
-        }
-    },
+    
     {
         id: 3,
         title: "University Registrar",
