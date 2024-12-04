7cf25b9f66440b119f18e8cbffcc2aa7f581c140
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-04 18:42:06 +0100

    Update ServicesList.jsx

--- frontend/src/Components/Nav/Services/ServicesList.jsx
+++ frontend/src/Components/Nav/Services/ServicesList.jsx
@@ -2,41 +2,31 @@ import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { NavContext } from '../useContext';
 import { useContext } from 'react';
 
-const ServicesList = ({ value, address, headerTitle }) => {
-    const navigate = useNavigate();
-    const { setMenu, setServices } = useContext(NavContext);
-
+const ServicesList = ({value, address, headerTitle}) => {
+const navigate=useNavigate();    
+  const {setMenu, setServices}=useContext(NavContext);
     return (
-        <div className="flex">
-            <div className="w-full">
-                <ul className="flex flex-col gap-6 pt-7 max-lg:px-0 max-lg:pt-3.5 font-500">
+        <div className='flex'>
+            <div className='w-full'>
+                <ul className=" flex flex-col gap-6 pt-7 max-lg:px-0 max-lg:pt-3.5 font-500">
                     <div>
-                        <Link
-                            to={address}
-                            onClick={() => {
-                                setServices(false);
-                            }}
-                            className="font-bold text-blue-800 text-md text-secondaryBlue"
-                        >
-                            {headerTitle
-                            }
-                        </Link>
+                        <Link to={address} onClick={()=>{setServices(false)}} className='font-bold text-blue-800 text-md text-secondaryBlue'>{headerTitle}</Link>
                     </div>
-                    {value?.map(({ title }, index) => (
-                        <NavLink
-                            to={`${address}/${title}`}
-                            onClick={() => {
-                                setMenu(false);
-                                setServices(false);
-                            }}
-                            className="[&.active]:font-medium [&.active]:text-blue-900 cursor-pointer font-thin hover:opacity-[.77] w-[100%]"
-                            key={index}
-                        >
-                            {title}
-                        </NavLink>
-                    ))}
+                    {
+                        value?.map(({ title }, index) => {
+                            return (
+                                <NavLink to={`${address}/${title}`} onClick={()=>{setMenu(false); setServices(false)}} className='[&.active]:font-medium [&.active]:text-blue-900 cursor-pointer font-thin hover:opacity-[.77] w-[100%]' key={index}>
+                                    
+                                        {title}
+                                    
+                                </NavLink>
+                            )
+                        })
+                    }
                 </ul>
-              {headerTitle === "Faculties"&& {/* See All Button */}
+                {/* See All Button */}
+                {headerTitle === "Faculties"
+                && 
                 <div className="mt-5 text-center">
                     <button
                         onClick={() => navigate(address)}
@@ -47,7 +37,7 @@ const ServicesList = ({ value, address, headerTitle }) => {
                 </div>}
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default ServicesList;
+export default ServicesList
