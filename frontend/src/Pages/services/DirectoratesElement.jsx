9dff9183a081196a17fca26ac288e0d7c4359a55
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-04 22:16:41 +0100

    Update DirectoratesElement.jsx

--- frontend/src/Pages/services/DirectoratesElement.jsx
+++ frontend/src/Pages/services/DirectoratesElement.jsx
@@ -55,10 +55,17 @@ const DirectoratesElement = () => {
                         {breadcrumbs}
                     </Breadcrumbs>
                 </Stack>
-                <div className='w-full flex items-center justify-center relative'>
-                    <img src={assets.wireframe} className='w-full ' />
-                    <h2 className='text-blue-700 text-2xl lg:text-4xl font-semibold z-0 absolute'>{data?.title}</h2>
-                </div>
+                <div className="relative w-full flex items-center justify-center min-h-20">
+    {/* Background Image */}
+    <img 
+        src={assets.wireframe} 
+        alt="Background" 
+        className="absolute w-full h-full z-0 object-cover" 
+    />
+    {/* Title */}
+    <h2 className="relative text-blue-700 text-lg sm:text-xl lg:text-2xl xl:text-4xl font-semibold z-10 text-center px-4">
+        {data?.title}
+    </h2>
 
             </div>
             {/* <div className='mx-4'>
