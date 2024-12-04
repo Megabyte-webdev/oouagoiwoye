334ca0c888def809496b90c7179759f2edf256cf
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-04 18:06:15 +0100

    Update Faculty.jsx

--- frontend/src/Pages/faculties/Faculty.jsx
+++ frontend/src/Pages/faculties/Faculty.jsx
@@ -50,12 +50,12 @@ export default function Faculty() {
                 </Breadcrumbs>
             </Stack>
             
-            <div className="relative w-full flex items-center justify-center min-h-[200px]">
+            <div className="relative w-full flex items-center justify-center min-h-28">
     {/* Background Image */}
     <img 
         src={assets.wireframe} 
         alt="Background" 
-        className="absolute w-full h-full object-cover inset-0 z-0" 
+        className="absolute w-full h-full inset-0 z-0" 
     />
     {/* Title */}
     <h2 className="relative text-blue-700 text-lg sm:text-xl lg:text-2xl xl:text-4xl font-semibold z-10 text-center px-4">
