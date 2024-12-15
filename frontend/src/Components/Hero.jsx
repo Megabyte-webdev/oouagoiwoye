820120a9b76f3f900a0f59dae5d6dadf9e20ef4a
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-15 07:52:53 -1100

    Update Hero.jsx

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <header className="hero-section p-5 md:p-10 bg-bgBlue">
       {/* <img className="rounded-3xl object-cover" src={assets.hero_img} alt="hero-img" /> */}
-     <div className="w-full h-full rounded-3xl relative bg-full bg-center bg-no-repeat" style={{ backgroundImage: `url(${assets.hero_img})` }} >
+     <div className="w-full h-full rounded-3xl relative bg-auto bg-center bg-no-repeat" style={{ backgroundImage: `url(${assets.hero_img})` }} >
         <div className='absolute top-0 bottom-0 left-0 w-full h-full rounded-3xl bg-gradient-to-r from-[#000000b5] from-50 to-transparent z-0' />
         <div className='absolute bottom-0 left-0 pl-5 lg:pl-10 pb-5 lg:pb-10'>
 
