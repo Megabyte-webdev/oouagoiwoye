51cca0e90cb8220403b579836127bc67bc0ce520
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-01-27 17:08:38 +0100

    Update PrincipalOfficers.jsx

--- frontend/src/Pages/administration/PrincipalOfficers.jsx
+++ frontend/src/Pages/administration/PrincipalOfficers.jsx
@@ -56,12 +56,12 @@ const PrincipalOfficers = () => {
         <div className='p-2 px-4 font-sans'>
             {principalData.map((item, index)=>(
                     <div key={index} className='w-full bg-white even:bg-blue-50 h-auto border-blue-900 border-t-2 last:border-b-2 p-2 flex items-center justify-between'>
-                        <div className='bg-blue-600 relative w-1/3 lg:w-2/12 h-60 flex items-center justify-center'>
-                            <div className='bg-yellow-400 w-20 lg:w-40 h-16 lg:h-36 absolute top-0 left-0 '></div>
-                            <figure className='w-[90%] h-[90%] relative'>
-                                <img src={item.image} className="w-full h-full object-cover" />
-                            </figure>
-                        </div>
+                                                   <div className='bg-blue-600 relative w-1/3 lg:w-2/12 h-auto flex items-center justify-center'>
+                                <div className='bg-yellow-400 w-20 lg:w-40 h-16 lg:h-36 absolute top-0 left-0 '></div>
+                                <figure className='m-1 lg:m-3 relative'>
+                                    <img src={item?.image} />
+                                </figure>
+                            </div>
                         <div className='w-10/12 h-full p-3 pl-5 lg:pl-10'>
                             <h1 className='text-royal text-xl lg:text-2xl font-bold'>
                                 {item.title}
