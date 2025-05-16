e56a081c99c1a4668330257455aced4bd1e572c4
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-05-16 22:25:34 +0100

    Update axiosinstance.js

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -1,23 +1,14 @@
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 
-export const axiosClient = AxiosInstance => {
-  let headers;
+export const axiosClient = ({ token, multiMedia = false }) => {
+  const contentType = multiMedia
+    ? "multipart/form-data"
+    : "application/json;charset=utf-8";
 
-  const contentType = multiMedia ? 'multipart/form-data' : "application/json;charset=utf-8"
-
-  if(token){
-   headers = {
-      "Content-Type": contentType,
-      "Authorization": `Bearer ${token}`,
-    }
-  } else{
-    headers = {
-      "Content-Type": contentType,
-      // "ngrok-skip-browser-warning":"true",
-    };
-
-  }
-
+  const headers = {
+    "Content-Type": contentType,
+    ...(token && { Authorization: `Bearer ${token}` }),
+  };
 
   const API = axios.create({
     baseURL: "https://api.oouweb.site/api/oouweb",
