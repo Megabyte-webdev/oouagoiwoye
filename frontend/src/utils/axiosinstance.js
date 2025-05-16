6c773e656fea805d26b4bd90e2781e2a618d071d
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-05-16 22:53:58 +0100

    Update axiosinstance.js

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -2,29 +2,27 @@ import axios from "axios";
 
 // Create an Axios instance
 const API = axios.create({
-baseURL: "https://api.oouweb.site/api/oouweb/",
-timeout: 60000,
-withCredentials: true,
+  baseURL: "/api/oouweb", 
+  withCredentials: true,  
 });
 
+
 API.interceptors.request.use(
-(config) => {
-const authToken = document.cookie
-.split("; ")
-.find((row) => row\.startsWith("authToken="))
-?.split("=")\[1];
+  (config) => {
+    const authToken = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("authToken="))
+      ?.split("=")[1]; 
 
-if (authToken) {
-  config.headers["Authorization"] = `Bearer ${authToken}`; 
-}
+    if (authToken) {
+      config.headers["Authorization"] = `Bearer ${authToken}`; 
+    }
 
-return config; 
-
-
-},
-(error) => {
-return Promise.reject(error);
-}
+    return config; 
+  },
+  (error) => {
+    return Promise.reject(error); 
+  }
 );
 
 export default API;
